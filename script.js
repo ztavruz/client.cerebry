0203c4ce80fa9069e0bf81cf11a27e628ec75afb
ztavruz <mr.ztavruz1987@mail.ru>
2019-05-15 10:43:30 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+window.onload = function()
+{
+	/*
+    axios.post('http://ServerCerebry/audioSession/create', {
+        name: 'Аудиосессия-1',
+        image: 'qweqweqwe',
+        description: 'Какоето классное описание',
+        cost: '123123'
+        // private $id;
+        // private $name;
+        // private $image;
+        // private $description;
+        // private $cost;
+        // private $audios = [];
+      })
+      .then(function (response) {
+        console.log(response);
+      });
+	  
+	  /audioSession/getAll
+	  */
+	 axios.get('http://ServerCerebry/audioSession/getAll')
+	.then(function (response) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+    // handle success
+    console.log(response);
+	});
+	
+}
