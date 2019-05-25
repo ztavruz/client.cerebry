85909e36cd75e36a4ea978355dee49888339aa23
ztavruz <ztavruz1987@mail.ru>
2019-05-25 16:48:36 +0300

    verstka addAudiosession ready

--- pages/audiosessions/script.js
+++ pages/audiosessions/script.js
@@ -1,23 +1,32 @@
 window.onload = function () {
-  axios.get('http://ServerCerebry/audioSession/getAll').then(function (response) {
-    // handle success
-    console.log(response);
-    let listAudiosessions = document.getElementById('audiosessions');
-    let arrAudiosessions = response.data;
+  // axios.get('http://ServerCerebry/audioSession/getAll').then(function (response) {
+  //   // handle success
+  //   console.log(response);
+  //   let listAudiosessions = document.getElementById('audiosessions');
+  //   let arrAudiosessions = response.data;
 
-    arrAudiosessions.forEach(function (audiosession) {
-      listAudiosessions.innerHTML +=
-      `<ul class="list-group  list-group-horizontal content__items ">
-        <li class="list-group-item flex-fill content__items_item"> ${audiosession.id}</li>
-        <li class="list-group-item flex-fill content__items_item">${audiosession.name}</li>
-        <li class="list-group-item flex-fill content__items_item">Дата</li>
-        <li class="list-group-item flex-fill content__items_item">Прослушать</li>
-      </ul>
-      <hr class="hr_audiosession" />
-      `;
-    });
+  //   arrAudiosessions.forEach(function (audiosession) {
+  //     listAudiosessions.innerHTML +=
+  //       `<ul class="list-group  list-group-horizontal content__items ">
+  //       <li class="list-group-item flex-fill content__items_item"> ${audiosession.id}</li>
+  //       <li class="list-group-item flex-fill content__items_item">${audiosession.name}</li>
+  //       <li class="list-group-item flex-fill content__items_item">Дата</li>
+  //       <li class="list-group-item flex-fill content__items_item">Прослушать</li>
+  //     </ul>
+  //     <hr class="hr_audiosession" />
+  //     `;
+  //   });
 
-  });
+  // });
+
+  let item__listen_menu = document.getElementById('item__listen_menu');
+  let listen__menu_block = document.getElementById('listen__menu_block');
+  console.log(item__listen_menu);
+
+  item__listen_menu.onclick = function () {
+    listen__menu_block.style.display = 'block';
+    console.log('......');
+  }
 
   let audiosessionName;
   let audiosessionImage;
